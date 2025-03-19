df17a4f9a3df5c469b9cbd0e20793a5db37d524f
AnhTuan1407 <anhtuan2003147@gmail.com>
2025-03-19 17:22:57 +0700

    feat: set calendar, find by location tour, find by profile name, modified: profile

--- src/routers/blog.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import blogController from "../controllers/blog.controller.js";
-
-const router = express.Router();
-
-router.post("/", blogController.createBlog);
-router.get("/", blogController.getAllBlogs);
-
-export default router;
